2f3867834fc4d25d21afeeb96077caf721d9cf6b
vikasb <vikas.bhargav@data-axle.com>
2024-07-20 12:59:38 +0530

    Added test cases

--- blog_application/blog_app/tests.py
+++ blog_application/blog_app/tests.py
@@ -1,3 +1,71 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
+from .models import Post, Comment
 
-# Create your tests here.
+
+def get_or_create_user() -> User:
+    """Create user and return user instance."""
+    user = User.objects.create_user(username='tester', password='test@123')
+    return user
+
+
+@pytest.mark.django_db
+def test_post_creation():
+    """Test creating a new post."""
+    user = get_or_create_user()
+    post = Post.objects.create(
+        title="Test Post",
+        content="Test Content",
+        author=user
+    )
+    assert post.title == "Test Post"
+    assert post.content == "Test Content"
+    assert post.author.id == user.id
+
+
+@pytest.mark.django_db
+def test_post_str():
+    """Test string representation of post."""
+    user = get_or_create_user()
+    post = Post.objects.create(
+        title="Test Post",
+        content="Test Content",
+        author=user
+    )
+    assert str(post) == "Test Post"
+
+
+@pytest.mark.django_db
+def test_comment_creation():
+    """Test creating a new comment."""
+    user = get_or_create_user()
+    post = Post.objects.create(
+        title="Test Post",
+        content="Test Content",
+        author=user
+    )
+    comment = Comment.objects.create(
+        post=post,
+        author=user,
+        text="Comment Text"
+    )
+    assert comment.post == post
+    assert comment.author == user
+    assert comment.text == "Comment Text"
+
+
+@pytest.mark.django_db
+def test_comment_str():
+    """Test string representation of comment."""
+    user = get_or_create_user()
+    post = Post.objects.create(
+        title="Test Post",
+        content="Test Content",
+        author=user
+    )
+    comment = Comment.objects.create(
+        post=post,
+        author=user,
+        text="Comment Text"
+    )
+    assert str(comment) == "Comment Text"
